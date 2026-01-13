9975885fe4c5d89d5101b91ea993b47e99bbb2b9
38309001 <38309001@L815WD220007.edu.cbe.ab.ca>
2026-01-13 15:36:21 -0700

    3:37

--- Chapter13/src/Skillbuilders/LinkedListTester.java
+++ Chapter13/src/Skillbuilders/LinkedListTester.java
@@ -1,3 +1,18 @@
+/*
+
+Program: Linked List Tester        Last Date of this Revision: January 13, 2026
+
+Purpose: A data structure that is an alternative too array lists, and is used to store data. This tester class tests the data structure.
+
+Author: Aiden Ducharme
+School: CHHS
+Course: CSE3110 - Iterative Algorithm 1
+ 
+
+*/
+
+
+
 package Skillbuilders;
 
 public class LinkedListTester {
@@ -7,18 +22,87 @@ public class LinkedListTester {
 	{
 		LinkedList list = new LinkedList();
 		
-		list.addAtFront("Red");
-		list.addAtFront("314");
-		list.addAtFront("Aiden");
+		list.addAtFront("Ben");
+		list.addAtFront("Cam");
+		list.addAtFront("Landen");
+		list.addAtFront("Elliot");
+		
+		System.out.println("This list has " + list.size() + " idems");
+		System.out.println(list);
+		
+		list.addatend("Berlin");
+		list.addatend("Tokyo");
+		list.addAtFront("Osaka");
+		list.addAtFront("Paris");
 		
 		System.out.println("List has " + list.size() + " idems");
 		System.out.println(list);
 		
-		list.addatend("Blue");
-		list.addatend("Blue");
+		list.makeEmpty();
+		System.out.println("The list was made empty");
+		
+		list.addAtFront("9888736353");
+		list.addAtFront("346366366466");
+		
 		
 		System.out.println("List has " + list.size() + " idems");
-		System.out.println(list);
+		System.out.println(list);;
 	}
 
 }
+
+
+//Test Cases:
+/*
+
+Test Case 1: 
+
+
+This list has 4 idems
+Hamza
+44
+Adam
+Aiden
+
+List has 8 idems
+Zachary
+34
+Hamza
+44
+Adam
+Aiden
+Ducharme
+Joel
+
+The list was made empty
+List has 2 idems
+567
+34
+
+Test Case 2:
+
+This list has 4 idems
+Elliot
+Landen
+Cam
+Ben
+
+List has 8 idems
+Paris
+Osaka
+Elliot
+Landen
+Cam
+Ben
+Berlin
+Tokyo
+
+The list was made empty
+List has 2 idems
+346366366466
+9888736353
+
+
+
+
+*/
--- Chapter13/src/Skillbuilders/QueueTester.java
+++ Chapter13/src/Skillbuilders/QueueTester.java
@@ -1,3 +1,17 @@
+/*
+
+Program: Queue tester clss         Last Date of this Revision: January 13, 2026
+
+Purpose: A type of array used to manage data in a program. This class tests this data structure.
+
+Author: Aiden Ducharme
+School: CHHS
+Course: CSE3110 - Iterative Algorithm 1
+ 
+
+*/
+
+
 package Skillbuilders;
 
 public class QueueTester {
@@ -5,19 +19,43 @@ public class QueueTester {
 	public static void main(String[] args) 
 	
 	{
-		Queue q1 = new Queue(3);
-		q1.enqueue("red");
-		q1.enqueue("Yellow");
-		q1.enqueue("Orange");
+		Queue q1 = new Queue(15);
+		q1.enqueue("Aiden");
+		q1.enqueue("does");
+		q1.enqueue("not");
+		q1.enqueue("love");
+		q1.enqueue("the");
+		q1.enqueue("mall");
 
 		System.out.println("Front of q1 is: " + q1.front());
 		System.out.println("size of q1 is: " + q1.size());
 		
-		q1.dequeue();
+		System.out.println("Now lets add some new elements");
+		q1.enqueue("unless");
+		q1.enqueue("he");
+		q1.enqueue("is");
+		q1.enqueue("with");
+		q1.enqueue("his");
+		q1.enqueue("friends!");
 		
-		System.out.println("Front of q1 is: " + q1.front());
-		System.out.println("size of q1 is: " + q1.size());
+	
+		
 		
 	}
 
 }
+
+
+/*Test Cases:
+ * 
+ * Test Case Number 1:
+ * 
+ * Front of q1 is: red
+size of q1 is: 3
+Front of q1 is: Yellow
+size of q1 is: 2
+
+
+Test Case Number 2:
+ */
+
