6e0b442a0acedb45f9bfc58029c1c4303546d6e4
38309001 <38309001@L815WD220007.edu.cbe.ab.ca>
2026-01-14 13:17:55 -0700

    1:17

--- Chapter13/src/Skillbuilders/QueueTester.java
+++ Chapter13/src/Skillbuilders/QueueTester.java
@@ -1,6 +1,6 @@
 /*
 
-Program: Queue tester clss         Last Date of this Revision: January 13, 2026
+Program: Queue tester class         Last Date of this Revision: January 13, 2026
 
 Purpose: A type of array used to manage data in a program. This class tests this data structure.
 
@@ -26,17 +26,24 @@ public class QueueTester {
 		q1.enqueue("love");
 		q1.enqueue("the");
 		q1.enqueue("mall");
+		q1.enqueue("45");
+		q1.enqueue("978");
+		q1.enqueue("56");
 
 		System.out.println("Front of q1 is: " + q1.front());
+		System.out.println("Is the qeueue empty: " + q1.isEmpty());
 		System.out.println("size of q1 is: " + q1.size());
 		
-		System.out.println("Now lets add some new elements");
+		
+		/* System.out.println("Now lets add some new elements");
 		q1.enqueue("unless");
 		q1.enqueue("he");
 		q1.enqueue("is");
 		q1.enqueue("with");
 		q1.enqueue("his");
 		q1.enqueue("friends!");
+		
+		*/
 		
 	
 		
@@ -57,5 +64,9 @@ size of q1 is: 2
 
 
 Test Case Number 2:
+
+Front of q1 is: Aiden
+Is the qeueue empty: false
+size of q1 is: 9
  */
 
--- Chapter13/src/Skillbuilders/TestStackP3S2.java
+++ Chapter13/src/Skillbuilders/TestStackP3S2.java
@@ -1,3 +1,14 @@
+/*
+Program: Stack tester class         Last Date of this Revision: January 13, 2026
+
+Purpose: A type of array used to manage data in a program. This class tests this data structure.
+
+Author: Aiden Ducharme
+School: CHHS
+Course: CSE3110 - Iterative Algorithm 1
+ */
+
+
 package Skillbuilders;
 
 public class TestStackP3S2 {
@@ -10,6 +21,8 @@ public class TestStackP3S2 {
 	s2.push("Red");
 	s2.push("Orange");
 	s2.push(11);
+	s2.push(67);
+	s2.push(98);
 	System.out.println("Top is pointing to: " + s2.top());
 	System.out.println("Size of the stack is: " + s2.size());
 	s2.pop();
@@ -17,9 +30,35 @@ public class TestStackP3S2 {
 	System.out.println("The top is ponting to: " + s2.top());
 	System.out.println("The size of the stack is: " + s2.size());
 	
+	s2.push(56);
+	s2.push(89);
 	
-	
+	System.out.println("The top is ponting to: " + s2.top());
+	System.out.println("The size of the stack is: " + s2.size());
 
 	}
 
 }
+
+/*
+ * Test Case Number 1
+ * 
+ * Top is pointing to: 11
+Size of the stack is: 3
+The top is ponting to: Orange
+The size of the stack is: 2
+
+ * Test Case number 2:
+ * 
+ * Top is pointing to: 11
+Size of the stack is: 3
+The top is ponting to: Orange
+The size of the stack is: 2
+The top is ponting to: 56
+The size of the stack is: 3
+ * 
+ * 
+ * 
+ * 
+ */
+
